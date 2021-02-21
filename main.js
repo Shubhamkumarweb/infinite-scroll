2fe50dc5d3cadf623508df1bbd8428c3b0fbbdee
Shubham Kumar <69460704+Shubhamkumarweb@users.noreply.github.com>
2021-02-21 16:19:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const container=document.querySelector(".container");
+const url="https://picsum.photos/id/";
+function getRandom(){
+ return Math.floor(Math.random()*300);
+}
+function loadImages(imagecount=10){
+for(let i=0;i<imagecount;i++)
+{
+    const img=document.createElement("img");
+    img.src=`${url}${getRandom()}/300/400`;
+    console.log(img.src)
+    container.appendChild(img);
+}
+}
+loadImages();
+window.addEventListener("scroll",()=>{
+    if(window.scrollY+window.innerHeight>=document.documentElement.scrollHeight){
+        loadImages();
+    }
+})
